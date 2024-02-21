418ce3bbdd810f83997ff9805e6ebf87420b7e40
Levente Orban <levente.orban@dyrector.io>
2024-02-21 15:55:59 +0100

    fix: small fix and remov .netlify folder from git cache

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.c73c08eb.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.c26a6efa.js"];
-export const stylesheets = ["_app/immutable/assets/0.722e6c00.css"];
-export const fonts = [];
--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.3ab6deb0.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.c26a6efa.js","_app/immutable/chunks/singletons.b0c33a64.js"];
-export const stylesheets = [];
-export const fonts = [];
